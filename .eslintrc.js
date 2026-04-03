eb8d78dd3070d125eb95609c930f3a6ff262dd04
its-me-prabin <prabinsaru0@gmail.com>
2026-04-03 09:44:24 +0545

    fix: Update ESLint config to support test globals and gulpfile requirements

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,17 +9,43 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'script',
   },
+  overrides: [
+    {
+      files: ['app/tests/**/*.js'],
+      env: {
+        mocha: true,
+      },
+      rules: {
+        'max-classes-per-file': 'off',
+      },
+    },
+    {
+      files: ['gulpfile.js'],
+      rules: {
+        'global-require': 'off',
+        'import/no-unresolved': 'off',
+      },
+    },
+  ],
   rules: {
     'no-undef': 'warn',
     'class-methods-use-this': 'off',
     'linebreak-style': 'off',
     'no-new': 'off',
-    'max-len': ['error', { code: 100, comments: 100 }],
+    'max-len': ['warn', { code: 100, comments: 100 }],
     'no-plusplus': 'off',
     'no-param-reassign': ['error', { props: false }],
   },
   globals: {
     requestAnimationFrame: 'readonly',
     cancelAnimationFrame: 'readonly',
+    Pacman: 'readonly',
+    Ghost: 'readonly',
+    Pickup: 'readonly',
+    CharacterUtil: 'readonly',
+    Timer: 'readonly',
+    SoundManager: 'readonly',
+    GameEngine: 'readonly',
+    GameCoordinator: 'readonly',
   },
 };
